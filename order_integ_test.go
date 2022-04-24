0430826f9252c4eec4755ab2e324947aff5049bd
Kevin Lee <363344+ipfans@users.noreply.github.com>
2022-04-24 14:46:38 +0800

    tests: Disable all outdated test cases

--- order_integ_test.go
+++ order_integ_test.go
@@ -3,112 +3,112 @@
 
 package tiktok_test
 
-import (
-	"context"
-	"encoding/json"
-	"os"
-	"testing"
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"os"
+// 	"testing"
 
-	"github.com/ipfans/tiktok"
-	"github.com/stretchr/testify/require"
-)
+// 	"github.com/ipfans/tiktok"
+// 	"github.com/stretchr/testify/require"
+// )
 
-func TestClient_GetOrderList_Integration(t *testing.T) {
-	c := newTestClient(t)
-	type args struct {
-		ak     string
-		shopID string
-		query  tiktok.GetOrderListRequest
-	}
-	tests := []struct {
-		name     string
-		args     args
-		wantList string
-		wantErr  bool
-	}{
-		{
-			name: "Request with valid ak",
-			args: args{
-				ak:    os.Getenv("AK"),
-				query: tiktok.GetOrderListRequest{},
-			},
-			wantList: loadTestData(t, "testdata/order/list/no_filter.json"),
-			wantErr:  false,
-		},
-		{
-			name: "Request with valid ak and filter",
-			args: args{
-				ak: os.Getenv("AK"),
-				query: tiktok.GetOrderListRequest{
-					OrderStatus: 111,
-				},
-			},
-			wantList: loadTestData(t, "testdata/order/list/filter_status_111.json"),
-			wantErr:  false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotList, err := c.GetOrderList(context.TODO(), tt.args.ak, tt.args.shopID, tt.args.query)
-			require.Equal(t, tt.wantErr, err != nil, "Client.GetOrderList() error = %v, wantErr %v", err, tt.wantErr)
-			if err != nil {
-				return
-			}
+// func TestClient_GetOrderList_Integration(t *testing.T) {
+// 	c := newTestClient(t)
+// 	type args struct {
+// 		ak     string
+// 		shopID string
+// 		query  tiktok.GetOrderListRequest
+// 	}
+// 	tests := []struct {
+// 		name     string
+// 		args     args
+// 		wantList string
+// 		wantErr  bool
+// 	}{
+// 		{
+// 			name: "Request with valid ak",
+// 			args: args{
+// 				ak:    os.Getenv("AK"),
+// 				query: tiktok.GetOrderListRequest{},
+// 			},
+// 			wantList: loadTestData(t, "testdata/order/list/no_filter.json"),
+// 			wantErr:  false,
+// 		},
+// 		{
+// 			name: "Request with valid ak and filter",
+// 			args: args{
+// 				ak: os.Getenv("AK"),
+// 				query: tiktok.GetOrderListRequest{
+// 					OrderStatus: 111,
+// 				},
+// 			},
+// 			wantList: loadTestData(t, "testdata/order/list/filter_status_111.json"),
+// 			wantErr:  false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			gotList, err := c.GetOrderList(context.TODO(), tt.args.ak, tt.args.shopID, tt.args.query)
+// 			require.Equal(t, tt.wantErr, err != nil, "Client.GetOrderList() error = %v, wantErr %v", err, tt.wantErr)
+// 			if err != nil {
+// 				return
+// 			}
 
-			b, _ := json.Marshal(gotList)
-			jsonData := string(b)
-			require.JSONEq(t, tt.wantList, jsonData)
-		})
-	}
-}
+// 			b, _ := json.Marshal(gotList)
+// 			jsonData := string(b)
+// 			require.JSONEq(t, tt.wantList, jsonData)
+// 		})
+// 	}
+// }
 
-func TestClient_GetOrderDetail_Integration(t *testing.T) {
-	t.SkipNow()
+// func TestClient_GetOrderDetail_Integration(t *testing.T) {
+// 	t.SkipNow()
 
-	c := newTestClient(t)
-	type args struct {
-		ak      string
-		shopID  string
-		orderID []string
-	}
-	tests := []struct {
-		name       string
-		args       args
-		wantDetail string
-		wantErr    bool
-	}{
-		{
-			name: "Request with valid ak and id",
-			args: args{
-				ak:      os.Getenv("AK"),
-				shopID:  os.Getenv("SHOPID"),
-				orderID: []string{"576467232825116133"},
-			},
-			wantDetail: loadTestData(t, "testdata/order/detail/id_576467232825116133.json"),
-			wantErr:    false,
-		},
-		{
-			name: "Request with valid ak and multi-id",
-			args: args{
-				ak:      os.Getenv("AK"),
-				shopID:  os.Getenv("SHOPID"),
-				orderID: []string{"576467232825116133", "576466303801001208"},
-			},
-			wantDetail: loadTestData(t, "testdata/order/list/filter_status_111.json"),
-			wantErr:    false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotDetail, err := c.GetOrderDetail(context.TODO(), tt.args.ak, tt.args.shopID, tt.args.orderID)
-			require.Equal(t, tt.wantErr, err != nil, "Client.GetOrderDetail() error = %v, wantErr %v", err, tt.wantErr)
-			if err != nil {
-				return
-			}
+// 	c := newTestClient(t)
+// 	type args struct {
+// 		ak      string
+// 		shopID  string
+// 		orderID []string
+// 	}
+// 	tests := []struct {
+// 		name       string
+// 		args       args
+// 		wantDetail string
+// 		wantErr    bool
+// 	}{
+// 		{
+// 			name: "Request with valid ak and id",
+// 			args: args{
+// 				ak:      os.Getenv("AK"),
+// 				shopID:  os.Getenv("SHOPID"),
+// 				orderID: []string{"576467232825116133"},
+// 			},
+// 			wantDetail: loadTestData(t, "testdata/order/detail/id_576467232825116133.json"),
+// 			wantErr:    false,
+// 		},
+// 		{
+// 			name: "Request with valid ak and multi-id",
+// 			args: args{
+// 				ak:      os.Getenv("AK"),
+// 				shopID:  os.Getenv("SHOPID"),
+// 				orderID: []string{"576467232825116133", "576466303801001208"},
+// 			},
+// 			wantDetail: loadTestData(t, "testdata/order/list/filter_status_111.json"),
+// 			wantErr:    false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			gotDetail, err := c.GetOrderDetail(context.TODO(), tt.args.ak, tt.args.shopID, tt.args.orderID)
+// 			require.Equal(t, tt.wantErr, err != nil, "Client.GetOrderDetail() error = %v, wantErr %v", err, tt.wantErr)
+// 			if err != nil {
+// 				return
+// 			}
 
-			b, _ := json.Marshal(gotDetail)
-			jsonData := string(b)
-			require.JSONEq(t, tt.wantDetail, jsonData)
-		})
-	}
-}
+// 			b, _ := json.Marshal(gotDetail)
+// 			jsonData := string(b)
+// 			require.JSONEq(t, tt.wantDetail, jsonData)
+// 		})
+// 	}
+// }
--- shop_integ_test.go
+++ shop_integ_test.go
@@ -3,54 +3,54 @@
 
 package tiktok_test
 
-import (
-	"context"
-	"encoding/json"
-	"os"
-	"testing"
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"os"
+// 	"testing"
 
-	"github.com/stretchr/testify/require"
-)
+// 	"github.com/stretchr/testify/require"
+// )
 
-func TestClient_GetAuthorizedShop_Integration(t *testing.T) {
-	c := newTestClient(t)
-	type args struct {
-		ak      string
-		shop_id string
-	}
-	b := loadTestData(t, "testdata/shop.json")
-	tests := []struct {
-		name      string
-		args      args
-		wantShops string
-		wantErr   bool
-	}{
-		{
-			name: "Request with ak",
-			args: args{
-				ak: os.Getenv("AK"),
-			},
-			wantShops: b,
-			wantErr:   false,
-		},
-		{
-			name: "Request with invalid ak",
-			args: args{
-				ak: os.Getenv("AK") + "111",
-			},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotShops, err := c.GetAuthorizedShop(context.TODO(), tt.args.ak, tt.args.shop_id)
-			require.Equal(t, tt.wantErr, err != nil, "Client.GetAuthorizedShop() error = %v, wantErr %v", err, tt.wantErr)
-			if err != nil {
-				return
-			}
-			b, _ := json.Marshal(gotShops)
-			jsonData := string(b)
-			require.JSONEq(t, tt.wantShops, jsonData)
-		})
-	}
-}
+// func TestClient_GetAuthorizedShop_Integration(t *testing.T) {
+// 	c := newTestClient(t)
+// 	type args struct {
+// 		ak      string
+// 		shop_id string
+// 	}
+// 	b := loadTestData(t, "testdata/shop.json")
+// 	tests := []struct {
+// 		name      string
+// 		args      args
+// 		wantShops string
+// 		wantErr   bool
+// 	}{
+// 		{
+// 			name: "Request with ak",
+// 			args: args{
+// 				ak: os.Getenv("AK"),
+// 			},
+// 			wantShops: b,
+// 			wantErr:   false,
+// 		},
+// 		{
+// 			name: "Request with invalid ak",
+// 			args: args{
+// 				ak: os.Getenv("AK") + "111",
+// 			},
+// 			wantErr: true,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			gotShops, err := c.GetAuthorizedShop(context.TODO(), tt.args.ak, tt.args.shop_id)
+// 			require.Equal(t, tt.wantErr, err != nil, "Client.GetAuthorizedShop() error = %v, wantErr %v", err, tt.wantErr)
+// 			if err != nil {
+// 				return
+// 			}
+// 			b, _ := json.Marshal(gotShops)
+// 			jsonData := string(b)
+// 			require.JSONEq(t, tt.wantShops, jsonData)
+// 		})
+// 	}
+// }
